d4c8820685d99ecb6a01caf4af6f8e702319916f
s31517 <s31517@pjwstk.edu.pl>
2024-10-08 15:35:36 +0200

    Added adder and subtractor

---
+++ src/Adder.java
@@ -0,0 +1,5 @@
+public class Adder {
+    static int add(int x, int y){
+        return x+y;
+    }
+}
---
+++ src/Subtractor.java
@@ -0,0 +1,5 @@
+public class Subtractor {
+    static int subtract(int x, int y){
+        return x -y;
+    }
+}
